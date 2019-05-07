59ef97388c84ffde09f2e5ff9a64d5c1e58048e4
Childcity <skulazkiy@gmail.com>
2019-05-07 17:31:44 +0300

    init .Net project. Add Data Access Layer

---
+++ DAL/Entities/Play.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NLayerApp.DAL.Entities {
+    public class Play
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Author { get; set; }
+		public string Genre { get; set; }
+		public DateTime DateTime { get; set; }
+
+		public virtual ICollection<Ticket> Tickets { get; set; }
+
+		public Play() {
+			Tickets = new HashSet<Ticket>();
+		}
+
+	}
+}
---
+++ DAL/Entities/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace NLayerApp.DAL.Entities { 
+	public class Ticket {
+		public int Id { get; set; }
+		public int Price { get; set; }
+		public int TotalCount { get; set; }
+		public int BoughtCount { get; set; }
+		public int BookedCount { get; set; }
+
+		public virtual ICollection<Play> Plays { get; set; }
+
+		public Ticket() {
+			Plays = new HashSet<Play>();
+		}
+	}
+}
